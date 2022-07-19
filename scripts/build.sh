8d49b82368d4767a44fabc0b3bc959293aede54a
yurii.badger <badger.lviv.ua@gmail.com>
2022-07-19 16:47:36 +0300

    [root] - CI/CD

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,18 +6,18 @@ echo Fetching environment variables
 cd ./apps/jill
 touch .env.production
 cat > .env.production <<EOF
-    REACT_APP_PRODUCTION=${1}
-    REACT_APP_API_URL=${2}
-    REACT_APP_FIREBASE_CONFIG=${3}
+REACT_APP_PRODUCTION=${1}
+REACT_APP_API_URL=${2}
+REACT_APP_FIREBASE_CONFIG=${3}
 EOF
 cd ../trixie
 
 touch .production.env
 cat > .production.env <<EOF
-    DATABASE_URL=${4}
-    PORT=${5}
-    FB_BUCKET_URL=${6}
-    FB_SERVICE_ACCOUNT=${7}
+DATABASE_URL=${4}
+PORT=${5}
+FB_BUCKET_URL=${6}
+FB_SERVICE_ACCOUNT=${7}
 EOF
 cd ../../
 

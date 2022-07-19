71ec13c63fa15f15d82f69241152d3fd11b38bd5
yurii.badger <badger.lviv.ua@gmail.com>
2022-07-19 15:57:53 +0300

    [root] - CI/CD

--- scripts/deploy-jill.sh
+++ scripts/deploy-jill.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-echo Pulling changes
-cd ./apps/jill
-touch .env.production
-echo Fetching environment variables
-cat > .env.production <<EOF
-    REACT_APP_PRODUCTION=${1}
-    REACT_APP_API_URL=${2}
-    REACT_APP_FIREBASE_CONFIG=${3}
-EOF
-cd ../../
-echo Installing dependencies
-yarn
-echo Building jill
-yarn build
 echo Deleting current jill build at /var/www/p-doc.com/jill
 rm -r /var/www/p-doc.com/jill/*
 echo Copying jill build to /var/www/p-doc.com/jill
--- scripts/deploy-trixie.sh
+++ scripts/deploy-trixie.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
-echo Pulling changes
-cd ./apps/trixie
-touch .production.env
-echo Fetching environment variables
-cat > .production.env <<EOF
-    DATABASE_URL=${1}
-    PORT=${2}
-    FB_BUCKET_URL=${3}
-    FB_SERVICE_ACCOUNT=${4}
-EOF
-cd ../../
-echo Installing dependencies
-yarn
+echo Killing pm2 process
 pm2 stop trixie
 pm2 kill
-echo Building trixie
-yarn build
-echo Starting Trixie
+echo Starting trixie
 yarn trixie-start-prod
 echo trixie deployed successfully

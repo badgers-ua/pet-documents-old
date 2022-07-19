5c8c7fb4038b51535c82254497c37ab5b8225f7b
yurii.badger <badger.lviv.ua@gmail.com>
2022-07-19 15:28:55 +0300

    [root] - CI/CD

--- apps/trixie/src/main.ts
+++ apps/trixie/src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 const graphqlUploadExpress = require('graphql-upload/graphqlUploadExpress.js');
 
 async function bootstrap(): Promise<void> {
-  console.log(1);
   const app = await NestFactory.create(AppModule);
   if (process.env.NODE_ENV == 'production') {
     const helmet = require('helmet');

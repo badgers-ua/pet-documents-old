c5b0783aaea320dfbf3eecd5818f8f4ca1db59e3
Yurii Yesipov <badger.lviv.ua@gmail.com>
2022-07-23 05:56:39 +0300

    [root, jill, trixie] - updated packages versions and GQL fragments (#15)
    * [root, jill, trixie] - updated packages versions
    
    * [jill] - GQL fragments

--- apps/jill/src/hooks/api/fragments.ts
+++ apps/jill/src/hooks/api/fragments.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client/core';
 
-export const PET_COMMON_FRAGMENT = gql`
-  fragment PetCommon on PetPreviewResDto {
+export const PET_PREVIEW_FRAGMENT = gql`
+  fragment PetPreview on PetPreviewResDto {
     _id
     name
     species
@@ -13,5 +13,39 @@ export const PET_COMMON_FRAGMENT = gql`
     notes
     weight
     breed
+  }
+`;
+
+export const PET_FRAGMENT = gql`
+  fragment Pet on PetResDto {
+    _id
+    name
+    species
+    gender
+    dateOfBirth
+    colour
+    notes
+    weight
+    avatar
+    owners {
+      _id
+      name
+      email
+    }
+    breed {
+      _id
+      name
+    }
+  }
+`;
+
+export const EVENT_FRAGMENT = gql`
+  fragment Event on EventResDto {
+    _id
+    type
+    petId
+    petName
+    date
+    description
   }
 `;
